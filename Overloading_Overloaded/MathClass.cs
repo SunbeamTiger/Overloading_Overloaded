46b094e77152533e0fd324ecd5db02696805aee7
danhagen <danhagen@outlook.com>
2022-04-18 09:23:30 -0700

    Add project files.

---
+++ Overloading_Overloaded/MathClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Overloaded
+{
+    class MathClass
+    {
+        public int Add (int num)
+        {
+           return  num + num;
+        }
+        public int Add(int num, int num2)
+        {
+            return num + num2;
+        }
+    }
+}
---
+++ Overloading_Overloaded/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Overloading_Overloaded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            MathClass mc = new MathClass();
+            result = mc.Add(3);
+            result = mc.Add(55, 190);
+           
+        }
+    }
+}
